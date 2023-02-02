917688ff881c80fc493f8d7c053abf454e52e4c2
peri <peri1908kor@gmail.com>
2023-02-02 17:08:47 +0200

    same account test done- problem on transactiondto when not same accounts are set

--- src/test/java/com/example/service/BankTransactionServiceImplTest.java
+++ src/test/java/com/example/service/BankTransactionServiceImplTest.java
@@ -1,15 +1,24 @@
 package com.example.service;
 
 import com.example.dto.TransactionDto;
+import com.example.entity.Account;
 import com.example.exception.InsufficientAmountException;
 import com.example.exception.SameSourceTargetAccException;
+import com.example.repository.AccountRepository;
 import com.example.utils.Currency;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BankTransactionServiceImplTest {
 
+    @Autowired
+    AccountRepository accountRepository;
     @Test
     void evalTransactionSameAcc() {
         BankTransactionServiceImpl service = new BankTransactionServiceImpl();
@@ -24,6 +33,12 @@ class BankTransactionServiceImplTest {
     void evalTransactionInsufficientAm() {
         BankTransactionServiceImpl service = new BankTransactionServiceImpl();
         TransactionDto transaction = new TransactionDto(3,4,3.5,Currency.EURO);
+        Account soure= new Account(3,1,Currency.EURO,"20/01/2019");
+        Account target= new Account(4,1,Currency.EURO,"20/01/2019");
+
+
+        Mockito.when(accountRepository.findById(3)).thenReturn(Optional.of(soure));
+        Mockito.when(accountRepository.findById(4)).thenReturn(Optional.of(target));
         assertThrows(InsufficientAmountException.class,
                 ()->{
                     service.evalTransaction(transaction);
